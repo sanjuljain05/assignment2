904c32f5248936213d56cf19c7b9bb509eca9870
sjain20 <sanjuljain05@gmail.com>
2017-02-10 08:56:49 -0600

    Added view.php

--- customer_view.php
+++ customer_view.php
@@ -2,34 +2,34 @@
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-  <meta name="description" content="">
-  <meta name="author" content="">
-  <link rel="icon" href="favicon.ico">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
 
-  <title>Add A Customer Form</title>
+    <title>Add A Customer Form</title>
 
-  <!-- Bootstrap core CSS -->
-  <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
 
-  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-  <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-  <!-- Custom styles for this template -->
-  <link href="styles.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="styles.css" rel="stylesheet">
 
-  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-  <!--[if lt IE 9]>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
 
 <body>
-
+    
   <!-- Fixed navbar -->
   <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container">
@@ -45,8 +45,8 @@
       </div>
       <div id="navbar" class="navbar-collapse collapse">
         <ul class="nav navbar-nav">
-          <li class="active"><a href="index.html">Home</a></li>
-          <li><a href="customer_view.php">View</a></li>
+          <li><a href="index.html">Home</a></li>
+          <li class="active"><a href="#">View</a></li>
           <li><a href="customer_add.html">Add</a></li>
           <!-- <li><a href="#contact">Contact</a></li>
             <li class="dropdown">
@@ -88,21 +88,78 @@
   </nav>
 
   <div class="container" style="padding-top: 80px">
+<?php // query.php
 
-    Welcome User, This is index page for assignment 2.
+// require_once 'login.php';
 
-  </div>
-  <!-- /container -->
+// login.php
+$hn = 'www.it354.com';
+$db = 'it354_students';
+$un = 'it354_students';
+$pw = 'steinway';
+
+$conn = new mysqli($hn, $un, $pw, $db);
+if ($conn->connect_error) die($conn->connect_error);
+
+$query = "SELECT * FROM customers";
+$result = $conn->query($query);
+if (!$result) die($conn->error);
+
+$rows = $result->num_rows; ?>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>Last/Company Name</th>
+            <th>First Name</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip</th>
+            <th>Email</th>
+            <th>Phone</th>
+        </tr>
+    </thead>
+    <tbody>
+      <?php
+        for ($j = 0 ; $j < $rows ; ++$j)
+{
+  $result->data_seek($j);
+  $row = $result->fetch_array(MYSQLI_ASSOC);
+?>
+  <tr>
+            <th><?php echo $j+1;?></th>
+            <td><?php echo $row['lastName']; ?></td>
+            <td><?php echo $row['firstName']; ?></td>
+            <td><?php echo $row['address']; ?></td>
+            <td><?php echo $row['city']; ?></td>
+            <td><?php echo $row['state']; ?></td>
+            <td><?php echo $row['zip']; ?></td>
+            <td><?php echo $row['email']; ?></td>
+            <td><?php echo $row['phone']; ?></td>
+            
+        </tr>
+<?php
+}
+$result->close(); 
+$conn->close(); 
+?>
+    </tbody>
+</table>
+</div>
+<!-- /container -->
 
 
-  <!-- Bootstrap core JavaScript
+<!-- Bootstrap core JavaScript
     ================================================== -->
-  <!-- Placed at the end of the document so the pages load faster -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
 <script src="js/bootstrap.min.js"></script>
 <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
 <script src="js/ie10-viewport-bug-workaround.js"></script>
+
+
 </body>
 
 </html>
